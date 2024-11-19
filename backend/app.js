9200047edef7053cba4d128afa8bccdf86677360
Shikhar Gupta <shikharguptagupta549@gmail.com>
2024-11-19 11:23:17 +0530

    Adding dotenv config

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
@@ -8,29 +9,16 @@ const contributionRoutes = require("./routes/contributionRoutes");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URI || "http://localhost:5173",
+    origin: process.env.FRONTEND_URI,
     credentials: true,
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URI || "http://localhost:5173");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // Respond to preflight requests
-  }
-
-  next();
-});
 
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
