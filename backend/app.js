70a14ea66d13b35a2e5fb446ac318c46844056c9
Shikhar Gupta <shikharguptagupta549@gmail.com>
2024-11-20 14:26:57 +0530

    Adding manual headers

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,23 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URI || "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // Respond OK for preflight
+  }
+
+  next();
+});
+
+
 
 
 app.use(express.json());
